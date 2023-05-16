62149701d063ff6cb6590a38aa57e11f5913f3cf
cr7st74n <cris-082011@hotmail.com>
2023-05-16 00:19:03 -0400

    finnish project practice  with constructure function

--- watch.js
+++ watch.js
@@ -1,20 +1,45 @@
 
 
 function Watch(){
-    this.duration = duration;
+
+    let startTime, endTime, running, duration = 0;
 
     this.start = function(){
-        const clock = setInterval()
+        if(running)
+            throw new Error("Watch has already started");
+
+        running = true;
+
+        startTime = new Date();
     }
     this.stop = function(){
+        if(!running)
+            throw new Error(" watch already stoped");
+        
+        running = false
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime())/ 1000;
+
+        duration+= seconds
 
     }
+    
     this.reset = function(){
+        startTime = 0;
+        endTime = 0;
+        running = null;
+        duration = 0;
+    }
+
+    //Read only property 
+    Object.defineProperty(this, "duration" , {
+        get: function(){
+            return duration
+        }
+    })
 
     }
-    this.timeLine = function(){
 
-    }
-}
 
-let time = new Date().setTime("3");
